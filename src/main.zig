1ea7a1274e9ccd3a59f02e965cbc7cbe058fb74a
mlycko <martin@mlycko.com>
2024-12-08 11:09:00 +0000

    Imported other project

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const testimport = @import("testmod");
 
 pub fn main() !void {
-    std.debug.print("Answer is {}\n", .{testimport.add(3, 7)});
+    var answer = testimport.add(3, 7);
+    answer = testimport.double(answer);
+    std.debug.print("Answer is {}\n", .{answer});
     std.debug.print("Compiled\n", .{});
 }
